e49d4b06e154d005a66710516774eda77bfa627a
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-12 08:52:53 +0200

    feat: chats endpoint and prettier

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world');
+import express from 'express';
+import { chats } from './db';
+
+const app = express();
+
+app.get('/_ping', (req, res) => {
+  res.send('pong');
+});
+
+app.get('/chats', (req, res) => {
+  res.json(chats);
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
