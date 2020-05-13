270033d06d9f2dbd913ad82a714536bd01fc2036
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-13 09:01:55 +0200

    feat: add routing to chat rooms screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+  RouteComponentProps
+} from 'react-router-dom';
+import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './components/ChatsListScreen';
 
 const App: React.FC = () => (
-  <div>
-    <ChatsListScreen />
-  </div>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/chats" component={ChatsListScreen} />
+      <Route
+        exact
+        path="/chats/:chatId"
+        component={({ match }: RouteComponentProps<{ chatId: string }>) => (
+          <ChatRoomScreen chatId={match.params.chatId} />
+        )}
+      />
+    </Switch>
+    <Route exact path="/" render={redirectToChats} />
+  </BrowserRouter>
 );
+
+const redirectToChats = () => <Redirect to="/chats" />;
 
 export default App;
