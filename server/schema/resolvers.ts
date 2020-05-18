6defe7e74c56535b91ecd412af8d62644a5913ef
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-18 20:58:59 +0200

    feat: use safe-api

--- server/schema/resolvers.ts
+++ server/schema/resolvers.ts
@@ -9,6 +9,8 @@ import { validateLength, validatePassword } from '../validators';
 import sql from 'sql-template-strings';
 import axios from 'axios';
 import { RandomPhoto } from '../types/unsplash';
+import { trackProvider } from '@safe-api/middleware';
+import { resolve } from 'path';
 
 const resolvers: Resolvers = {
   Date: DateTimeResolver,
@@ -75,20 +77,42 @@ const resolvers: Resolvers = {
 
       if (participant && participant.picture) return participant.picture;
 
+      interface RandomPhotoInput {
+        query: string;
+        orientation: 'landscape' | 'portrait' | 'squarish';
+      }
+
+      const trackedRandomPhoto = await trackProvider(
+        async ({ query, orientation }: RandomPhotoInput) =>
+          (
+            await axios.get<RandomPhoto>(
+              'https://api.unsplash.com/photos/random',
+              {
+                params: {
+                  query,
+                  orientation,
+                },
+                headers: {
+                  Authorization:
+                    `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+                },
+              }
+            )
+          ).data,
+        {
+          provider: 'Unsplash',
+          method: 'RandomPhoto',
+          location: resolve(__dirname, '../logs/main'),
+        }
+      );
+
       try {
-        return (await axios.get<RandomPhoto>(
-          'https://api.unsplash.com/photos/random',
-          {
-            params: {
-              query: 'portrait',
-              orientation: 'squarish',
-            },
-            headers: {
-              Authorization:
-                `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
-            },
-          }
-        )).data.urls.small;
+        return (
+          await trackedRandomPhoto({
+            query: 'portrait',
+            orientation: 'squarish',
+          })
+        ).urls.small;
       } catch (err) {
         console.error('Cannot retrieve random photo:', err);
         return null;
