5ce7c841cd6011b38a620a5d76a0a37f4856ae93
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-19 08:13:21 +0200

    feat(server): use graphql modules

--- server/modules/common/index.ts
+++ server/modules/common/index.ts
@@ -1,25 +1,53 @@
+import { GraphQLModule } from '@graphql-modules/core';
 import { gql } from 'apollo-server-express';
 import { DateTimeResolver, URLResolver } from 'graphql-scalars';
+import { pool } from '../../db';
 import { Resolvers } from '../../types/graphql';
 
-export const typeDefs = gql`
+const { PostgresPubSub } = require('graphql-postgres-subscriptions');
+
+const typeDefs = gql`
   scalar Date
   scalar URL
-
   type Query {
     _dummy: Boolean
   }
-
   type Mutation {
     _dummy: Boolean
   }
-
   type Subscription {
     _dummy: Boolean
   }
 `;
 
-export const resolvers: Resolvers = {
+const resolvers: Resolvers = {
   Date: DateTimeResolver,
   URL: URLResolver,
 };
+
+const pubsub = new PostgresPubSub({
+  host: 'localhost',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  user: 'testuser',
+  password: 'testpassword',
+  database: 'whatsapp',
+});
+
+export default new GraphQLModule({
+  name: 'common',
+  typeDefs,
+  resolvers,
+  async context({ res, connection }) {
+    let db;
+
+    if (!connection) {
+      db = await pool.connect();
+    }
+
+    return {
+      pubsub,
+      res,
+      db,
+    };
+  },
+});
