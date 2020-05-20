1460abbd8475b2bcd88af34047f984c11f7ce1e0
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-20 22:05:20 +0200

    feat: produce transpiled code

--- server/modules/chats/chats.provider.ts
+++ server/modules/chats/chats.provider.ts
@@ -145,7 +145,9 @@ export class Chats {
     messages.reverse();
 
     // cursor is a number representation of created_at
-    const cursor = messages.length ? new Date(messages[0].created_at).getTime() : 0;
+    const cursor = messages.length
+      ? new Date(messages[0].created_at).getTime()
+      : 0;
     const { rows: next } = await this.db.query(
       sql`SELECT * FROM messages WHERE chat_id = ${chatId} AND created_at < ${cursorToDate(
         cursor
@@ -326,5 +328,5 @@ export class Chats {
 }
 
 function cursorToDate(cursor: number) {
-  return `'${format(cursor, 'YYYY-MM-DD HH:mm:ss')}'`;
+  return `'${format(cursor, 'yyyy-MM-dd HH:mm:ss')}'`;
 }
